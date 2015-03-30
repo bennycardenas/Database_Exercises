2b50abddb29efe010b1672bd9787c8fc52d519dc
Benny Cardenas <benitocardenas@gmail.com>
2015-03-30 08:56:49 -0500

    First mysql DB

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    Artist VARCHAR(100) NOT NULL,
+    Album_Release VARCHAR(100) NOT NULL,
+    Release_Year YEAR(4) NOT NULL,
+    Sales_in_Millions FLOAT NOT NULL,
+    Genre VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
