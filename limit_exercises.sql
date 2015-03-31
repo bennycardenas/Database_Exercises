ee2300bfa2271c83a049611275b97642bfd4cf16
Benny Cardenas <benitocardenas@gmail.com>
2015-03-31 13:51:02 -0500

    LIMIT

---
+++ limit_exercises.sql
@@ -0,0 +1,18 @@
+SELECT DISTINCT last_name
+FROM employees 
+GROUP BY last_name DESC
+LIMIT 10;
+
+SELECT * 
+FROM employees
+WHERE birth_date LIKE '%12-25'
+AND hire_dat BETWEEN '1990-01-01' AND '1999-12-31'
+ORDER BY birth_date ASC, hire_date DESC
+LIMIT 5;
+
+SELECT * 
+FROM employees
+WHERE birth_date LIKE '%12-25'
+AND hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+ORDER BY birth_date ASC, hire_date DESC
+LIMIT 5 OFFSET 45;
