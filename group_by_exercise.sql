d307304c499390240fbacfddc0568e011b595c7c
Benny Cardenas <benitocardenas@gmail.com>
2015-03-31 13:23:17 -0500

    REFACTORED with multiple GROUP BYs and SELECTs

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -5,16 +5,33 @@ SELECT DISTINCT title
 FROM titles
 ORDER BY title ASC;
 
+/* This is same as above */
+SELECT title 
+FROM titles
+GROUP BY title ASC;
+
 SELECT last_name
 FROM employees
 WHERE last_name LIKE 'E%'
 AND last_name LIKE '%E'
 GROUP BY last_name;
 
-SELECT last_name, first_name
+SELECT last_name
 FROM employees
 WHERE last_name LIKE 'E%'
 AND last_name LIKE '%E'
+GROUP BY last_name;
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+AND last_name LIKE '%E'
+GROUP BY last_name;
+
+/* This is same as above */
+SELECT last_name, first_name
+FROM employees
+WHERE last_name LIKE 'E%E'
 GROUP BY last_name, first_name;
 
 SELECT last_name
