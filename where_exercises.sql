7f795489b4af6342d47db2632c9f907cc000f50c
Benny Cardenas <benitocardenas@gmail.com>
2015-03-31 11:38:01 -0500

    ORDER BY oldest and last hired

--- where_exercises.sql
+++ where_exercises.sql
@@ -4,15 +4,13 @@ WHERE first_name = 'Irena'
 OR first_name = 'Vidya'
 OR first_name = 'Maya'; */
 
-SELECT *
+/* SELECT *
 FROM employees
 WHERE gender = 'M'
 AND (first_name = 'Irena'
 OR first_name = 'Vidya'
 OR first_name = 'Maya')
-ORDER BY last_name, first_name;
-
-
+ORDER BY last_name, first_name; */
 
 /* SELECT *
 FROM employees
@@ -32,24 +30,28 @@ FROM employees
 WHERE last_name LIKE 'E%'
 AND last_name LIKE '%E'; */
 
-/* SELECT *
+SELECT *
 FROM employees
 WHERE hire_date
 BETWEEN '1990-01-01' AND '1999-12-31'
-AND birth_date LIKE '%-12-25'; */
+AND birth_date LIKE '%-12-25'
+ORDER BY birth_date, hire_date DESC;
+
 
 /* SELECT *
 FROM employees
 WHERE last_name LIKE '%q%'
 AND last_name NOT LIKE '%qu%'; */
 
-/* SELECT * FROM employees
+SELECT * FROM employees
 WHERE hire_date 
 BETWEEN '1990-01-01' AND '1999-12-31';
 
-SELECT first_name
+/*SELECT first_name
 FROM employees
-WHERE birth_date LIKE '%-12-25';
+WHERE birth_date LIKE '%-12-25';*/
 
-SELECT * FROM employees WHERE LAST_name LIKE 'E%'; */
+/* SELECT * FROM employees 
+WHERE LAST_name LIKE 'E%'
+ORDER BY emp_no DESC; */
 
