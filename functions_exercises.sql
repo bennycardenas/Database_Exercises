277b153d12b80df33b292b92e771578c43610020
Benny Cardenas <benitocardenas@gmail.com>
2015-03-31 15:42:07 -0500

    REFACTORED FUNCTIONS completed

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -17,7 +17,7 @@ FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya')
 AND gender = 'M'; */
 
-SELECT count(*)
+SELECT count(*), gender
 FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya')
 GROUP BY gender;
@@ -38,18 +38,20 @@ FROM employees
 WHERE last_name LIKE 'E%'
 AND last_name LIKE '%E'; */
 
-SELECT DATEDIFF(CURDATE(), hire_date) AS 'Days w/ Co'
+SELECT first_name, last_name, DATEDIFF(CURDATE(), hire_date) AS 'Days w/ Co'
 FROM employees
 WHERE hire_date
 BETWEEN '1990-01-01' AND '1999-12-31'
 AND birth_date LIKE '%-12-25'
-ORDER BY birth_date, hire_date DESC;
+ORDER BY hire_date DESC;
 
 
-/* SELECT *
+SELECT first_name, last_name , count(first_name) AS '# ppl with this Name'
 FROM employees
 WHERE last_name LIKE '%q%'
-AND last_name NOT LIKE '%qu%'; */
+AND last_name NOT LIKE '%qu%'
+GROUP BY first_name, last_name
+ORDER BY count(*) DESC;
 
 SELECT * FROM employees
 WHERE hire_date 
