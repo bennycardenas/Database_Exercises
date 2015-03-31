d92cbfdc529faf612a4770fd258dce76796441b5
Benny Cardenas <benitocardenas@gmail.com>
2015-03-31 11:52:23 -0500

    SELECT DISTINCT in GROUP BY exercise

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,1 +1,13 @@
 use codeup_test_db;
+
+delete
+    from albums 
+    where release_date > 1991;
+
+delete
+    from albums 
+    where genre LIKE 'disco';
+x
+delete
+    from albums 
+    where artist = 'Whitney Houston';
