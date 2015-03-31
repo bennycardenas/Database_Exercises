7fb27e2782ce3537a1fe889864064f24bd0d4e1f
Benny Cardenas <benitocardenas@gmail.com>
2015-03-31 10:19:48 -0500

    WHERE statements with NOT LIKE

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,12 +1,39 @@
+/* SELECT *
+FROM employees
+WHERE first_name = 'Irena'
+OR first_name = 'Vidya'
+OR first_name = 'Maya'; */
+
+/* SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+AND gender = 'M'; */
+
+/* SELECT * FROM employees
+WHERE last_name LIKE '%q%';*/
+
+/* SELECT *
+FROM employees
+WHERE last_name LIKE 'E%'
+OR last_name LIKE '%E'; */
+
+/* SELECT *
+FROM employees
+WHERE last_name LIKE 'E%'
+AND last_name LIKE '%E'; */
+
+/* SELECT *
+FROM employees
+WHERE hire_date
+BETWEEN '1990-01-01' AND '1999-12-31'
+AND birth_date LIKE '%-12-25'; */
+
 SELECT *
 FROM employees
-WHERE first_name
-IN ('Irena', 'Vidya', 'Maya');
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%';
 
-SELECT * FROM employees
-WHERE last_name LIKE '%q%';
-
-SELECT * FROM employees
+/* SELECT * FROM employees
 WHERE hire_date 
 BETWEEN '1990-01-01' AND '1999-12-31';
 
@@ -14,5 +41,5 @@ SELECT first_name
 FROM employees
 WHERE birth_date LIKE '%-12-25';
 
-SELECT * FROM employees WHERE LAST_name LIKE 'E%';
+SELECT * FROM employees WHERE LAST_name LIKE 'E%'; */
 
