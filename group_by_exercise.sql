eedf37f9030bfa9d3f53d980210b08e8a121a21c
Benny Cardenas <benitocardenas@gmail.com>
2015-03-31 13:09:53 -0500

    with multiple GROUP BYs and SELECTs

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -11,4 +11,14 @@ WHERE last_name LIKE 'E%'
 AND last_name LIKE '%E'
 GROUP BY last_name;
 
+SELECT last_name, first_name
+FROM employees
+WHERE last_name LIKE 'E%'
+AND last_name LIKE '%E'
+GROUP BY last_name, first_name;
 
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
