e9e4d091838a79e818661eed4424e1d02174b986
Benny Cardenas <benitocardenas@gmail.com>
2015-03-31 12:01:16 -0500

    last names starting AND ending with E

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -1,2 +1,14 @@
 SELECT DISTINCT title 
 FROM titles;
+
+SELECT DISTINCT title 
+FROM titles
+ORDER BY title ASC;
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+AND last_name LIKE '%E'
+GROUP BY last_name;
+
+
