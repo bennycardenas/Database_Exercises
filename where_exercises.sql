313e63f5fe66eddb5984a7d0a5e27815a3305b17
Benny Cardenas <benitocardenas@gmail.com>
2015-03-31 09:52:14 -0500

    WHERE statements

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,2 +1,18 @@
--- SELECT * FROM employees WHERE hire_date = '1990-10-22';
-SELECT * FROM employees WHERE emp_no = 101010;
+SELECT *
+FROM employees
+WHERE first_name
+IN ('Irena', 'Vidya', 'Maya');
+
+SELECT * FROM employees
+WHERE last_name LIKE '%q%';
+
+SELECT * FROM employees
+WHERE hire_date 
+BETWEEN '1990-01-01' AND '1999-12-31';
+
+SELECT first_name
+FROM employees
+WHERE birth_date LIKE '%-12-25';
+
+SELECT * FROM employees WHERE LAST_name LIKE 'E%';
+
